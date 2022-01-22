fb266089ecb8152623b5875b4e8fc68b1fa75f35
Jorrin <jorrinkievit@hotmail.com>
2022-01-22 20:42:57 +0100

    Update play.ts

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -31,6 +31,8 @@ export const command: DiscordCommand = {
         const member = guild?.members.cache.get(interaction.member?.user.id!);
         const voiceChannel = member?.voice.channel;
 
+        console.log("voiceLine", voiceLine);
+
         if (voiceChannel) {
           const connection = joinVoiceChannel({
             channelId: voiceChannel.id,
@@ -43,12 +45,40 @@ export const command: DiscordCommand = {
             entersState(connection, VoiceConnectionStatus.Ready, 5_000);
 
           const audioplayer = createAudioPlayer();
+
           connection.subscribe(audioplayer);
 
           const audioFile = createAudioResource(voiceLine.voiceFile.asset.url);
 
           audioplayer.play(audioFile);
           entersState(audioplayer, AudioPlayerStatus.Playing, 5_000);
+
+          connection.on("stateChange", (oldState, newState) =>
+            console.log(
+              `CONNECTION: oldstate: ${oldState}, newstate: ${newState}`
+            )
+          );
+          connection.on("debug", (message) => {
+            console.log("CONNECTION_DEBUG", message);
+          });
+          connection.on("error", () => {
+            console.log("ER IS EEN ERROR OP DE CONNECTIE");
+          });
+
+          audioplayer.on("stateChange", (oldState, newState) => {
+            console.log(
+              `AUDIOPLAYER: oldstate: ${oldState}, newstate: ${newState}`
+            );
+          });
+          audioplayer.on("subscribe", () => {
+            console.log("SUBSCRIBED AUDIOPLAYER");
+          });
+          audioplayer.on("unsubscribe", () => {
+            console.log("UNSUBSCRIBED");
+          });
+          audioplayer.on("debug", (message) => {
+            console.log("AUDIOPLAYER_DEBUG", message);
+          });
 
           audioplayer.on(AudioPlayerStatus.Idle, () => {
             connection.destroy();
